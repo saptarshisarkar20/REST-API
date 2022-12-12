6c1d4f826f319cdc02ea2b6135e3b5cb8378e4a5
saptarshisarkar20 <saptarshi2017cob@gmail.com>
2022-12-12 16:29:45 +0530

    to be continues after learning express

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+
+//TODO: TO BE CONTINUED AFTER LEARNING EXPRESS JS
+//? YOUTUBE LINK HERE
+//! https://www.youtube.com/playlist?list=PLwGdqUZWnOp1P9xSsJg7g3AY0CUjs-WOa
+//* CLOSING NOW 
