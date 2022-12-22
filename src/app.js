52103a912c6a6d8252cf12ffb1ad8fb9ac59bc5a
saptarshisarkar20 <saptarshi2017cob@gmail.com>
2022-12-22 15:46:59 +0530

    learned post search and delete

--- src/app.js
+++ src/app.js
@@ -17,20 +17,90 @@ app.get("/", (req, res) => {
     res.send("this is home page");
 });
 
-app.post("/students", (req, res) => {
-    console.log(req.body);
-    const user = new Student(req.body);
+// app.post("/students", (req, res) => {
+//     console.log(req.body);
+//     const user = new Student(req.body);
 
-    //* SAVING USING PROMISE
-    user.save()
-        .then(() => {
-            res.status(201).send(user);
-        })
-        .catch((e) => {
-            res.status(400).send(e);
-        });
+//     //* SAVING USING PROMISE
+//     user.save()
+//         .then(() => {
+//             res.status(201).send(user);
+//         })
+//         .catch((e) => {
+//             res.status(400).send(e);
+//         });
 
-    // res.send("hello from");
+//     // res.send("hello from");
+// });
+
+//* CREATE STUDENTS DATA
+app.post("/students", async (req, res) => {
+    try {
+        console.log(req.body);
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+//* GET ALL STUDENTS DATA
+app.get("/students", async (req, res) => {
+    try {
+        const studentsData = await Student.find();
+        console.log(studentsData);
+        res.send(studentsData);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// //* GET INDIVIDUAL STUDENTS DATA BY ID
+// app.get("/students/:id", async (req, res) => {
+//     try {
+//         const _id = req.params.id;
+//         // res.send(_id);
+//         const stuData = await Student.findById({ _id: _id });
+//         if (!stuData) {
+//             res.status(404).send();
+//         } else {
+//             res.send(stuData);
+//         }
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+//* GET INDIVIDUAL STUDENTS DATA BY NAME
+app.get("/students/:name", async (req, res) => {
+    try {
+        const _name = req.params.name;
+        // res.send(_name);
+        const stuName = await Student.find({ name: _name });
+        if (!stuName) {
+            res.status(404).send();
+        } else {
+            res.send(stuName);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+//* DELETE STUDENTS BY ID
+app.delete("/students/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const stuDelete = await Student.findByIdAndDelete({ _id: id });
+        if (!stuDelete) {
+            res.status(404).send();
+        } else {
+            res.send(stuDelete);
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 app.listen(port, () => {
